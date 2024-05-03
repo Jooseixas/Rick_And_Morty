b08f8d8db87baf4520e46f95b0441c41747b4acd
João Seixas <127799889+Jooseixas@users.noreply.github.com>
2024-05-03 20:21:22 -0300

    Add files via upload

---
+++ RickAndMorty/src/views/AboutView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <h1 class="mb-4 text-center">Meus dados</h1>
+    <div>
+      <h2>Nome:</h2>
+      <p>João Victor Marques Seixas</p>
+    </div>
+    <div>
+      <h2>RA:</h2>
+      <p>1974858</p>
+    </div>
+    <div>
+      <h2>Univesidade:</h2>
+      <p>UNIMAR</p>
+    </div>
+    <div>
+      <h2>Curso:</h2>
+      <p>Análise e desenvolvimento de sistemas</p>
+    </div>
+  </div>
+</template>
